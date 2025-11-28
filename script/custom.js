de174d453f691dac04503822ab8c99346ff451b0
Etienne Ferry <etienne.ferry@ccicampus.fr>
2025-11-28 16:48:57 +0100

    lightboxGalery add, increasing font size on navBar, containing somme picture n alot of suffer

--- script/custom.js
+++ script/custom.js
@@ -30,3 +30,77 @@ document.addEventListener("click", (e) => {
         sideNavBar.setAttribute("aria-hidden", "true");
     }
 });
+
+//GALERIE
+
+
+const images = document.querySelectorAll('.gallery-img');
+const lightbox = document.getElementById('lightbox');
+const lightboxImg = document.getElementById('lightbox-img');
+const closeGalBtn = document.querySelector('.close-lightbox');
+const nextBtn = document.querySelector('.next');
+const prevBtn = document.querySelector('.prev');
+
+let currentIndex = 0; 
+
+//  Fonction ouverture lightbox
+images.forEach((img, index) => {
+    img.addEventListener('click', () => {
+        lightbox.style.display = "flex";
+        lightboxImg.src = img.src; 
+        currentIndex = index; 
+        document.body.style.overflow = "hidden"; 
+    });
+});
+
+// Fonction fermerture lightbox
+function closeLightbox() {
+    lightbox.style.display = "none";
+    document.body.style.overflow = "auto"; 
+}
+
+closeBtn.addEventListener('click', closeLightbox);
+
+//Clique endehors pour fermer
+lightbox.addEventListener('click', (e) => {
+    if (e.target === lightbox) {
+        closeLightbox();
+    }
+});
+
+//  Navigation 
+function changeImage(direction) {
+    currentIndex += direction;
+
+    // Si on arrive à la fin, on revient au début (boucle)
+    if (currentIndex >= images.length) {
+        currentIndex = 0;
+    }
+    // Si on est au début et qu'on recule, on va à la fin
+    if (currentIndex < 0) {
+        currentIndex = images.length - 1;
+    }
+
+    // Mise à jour de l'image
+    lightboxImg.src = images[currentIndex].src;
+}
+
+// Attacher les événements aux boutons prev/next
+nextBtn.addEventListener('click', (e) => {
+    e.stopPropagation(); // Évite de fermer la lightbox quand on clique sur la flèche
+    changeImage(1);
+});
+
+prevBtn.addEventListener('click', (e) => {
+    e.stopPropagation();
+    changeImage(-1);
+});
+
+// 5. Navigation au clavier (Flèches et Echap)
+document.addEventListener('keydown', (e) => {
+    if (lightbox.style.display === "block") {
+        if (e.key === "ArrowLeft") changeImage(-1);
+        if (e.key === "ArrowRight") changeImage(1);
+        if (e.key === "Escape") closeLightbox();
+    }
+});
